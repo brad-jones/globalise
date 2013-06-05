752a5d19ec1850d9d1c0d9b7e15ca3bf2b65c5ec
Brad Jones <bj@gravit.com.au>
2013-06-05 11:09:42 +1000

    Intial Commit

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
 globalise
 =========
 
-Node.js Globalise Module
+Okay so firstly lets just get this out of the way, I am a PHP dev and I am
+by no means a javascript expert. However I am having a play with node.js
+for the first time... so please be gentle :)
+
+To make my life more pleasurable in node I have created this
+module that basically globalises everything, in effect providing
+autoloading like I am used to in PHP. I hated having all these
+require statements everywhere.
+
+Feel free to send through as many death threats as you like but my new haraka
+based mail server will filter them out... hahaha
+
+How To Use
+==========
+
+	1. npm install globalise
+	
+	2. In your main.js:
+	   require('globalise');
+	
+	3. Now everything is global, you can call up say
+	   a path function without having to actually require path.
+
+	   console.log(path.resolve('./'));
+
+	   It just works!
+
+*Okay so thats pretty neat, did I also mention we will autoload all your
+installed modules but only from your applications node_modules folder.
+Not from the global modules and we only go one level deep.*
+
+What Else Do You Get
+====================
+So as if that wasn't enough I also include the node module "classical" for you.
+You can check out it's docs for what it's all about:
+https://npmjs.org/package/classical
+
+We add on top of this and create a Namespace function. Again this is Global.
+
+You can use it like so:
+
+	./lib/brads/cool/namespace/foo.js - contains
+	Namespace('brads.cool.namespace.foo', function(){ console.log('foo'); });
+
+	./main.js - contains
+	require('globalise');
+	require('./lib/brads/cool/namespace/foo.js');
+	brads.cool.namespace.foo();
+
+But this isn't autoloaded at the moment, to do this you need to tell us where
+your "lib" folder is that contains your javascript classes and functions all
+nicely namespaced similar to what you might find in a PHP project.
+Okay and now for the autoloading part, same example as above:
+
+	
+	./lib/brads/cool/namespace/foo.js - contains
+	Namespace('brads.cool.namespace.foo', function(){ console.log('foo'); });
+
+	./main.js - contains
+	require('globalise');
+	globalise.autoload('./lib');
+	brads.cool.namespace.foo();
+
+*globalise.autoload is syncronous*
+
+**Yay no more requires ever again!!!**
+
+The Fine Print
+==============
+I'll admit this probably has far reaching implications that I am unaware of
+just yet. But it does seem to be working for me just fine and dandy for the
+time being. I take no responsibility for the "security" or "performance"
+implications this may or may not have on your application.
---
+++ index.js
@@ -0,0 +1,123 @@
+////////////////////////////////////////////////////////////////////////////////
+//                    __        ___            __   __                          
+//              ____ |  |   ____\_ |__ _____  |  | |__| ______ ____             
+//             / ___\|  |  /  _ \| __ \\__  \ |  | |  |/  ___// __ \            
+//            / /_/  >  |_(  <_> ) \_\ \/ __ \|  |_|  |\___ \\  ___/            
+//            \___  /|____/\____/|___  (____  /____/__/____  >\___  >           
+//           /_____/                 \/     \/             \/     \/            
+// =============================================================================
+// -------- Designed and Developed by Brad Jones <brad @="bjc.id.au" /> --------
+// =============================================================================
+////////////////////////////////////////////////////////////////////////////////
+
+// First up lets bring in our deps
+require('classical');
+var file = require('file');
+
+/**
+ * Function: Namespace
+ * =============================================================================
+ * We are going to add another language construct to the global scope.
+ * You can use it like so:
+ * 
+ * brads/cool/namespace/foo.js
+ * Namespace('brads.cool.namespace.foo', function(){ console.log('foo'); });
+ * 
+ * main.js
+ * require('brads/cool/namespace/foo.js');
+ * brads.cool.namespace.foo();
+ * 
+ * Parameters:
+ * -----------------------------------------------------------------------------
+ * name - the full name of the class, function or object
+ * value - the class, function or object
+ * 
+ * Returns:
+ * -----------------------------------------------------------------------------
+ * void
+ */
+global.Namespace = function(name, value)
+{
+	// Add the new namespace to the global scope
+	var base = global;
+	
+	// Split the name up
+	var names = name.split('.');
+	
+	// Remove the last name and keep it for later
+	var lastName = names.pop();
+	
+	// Walk the hierarchy, creating new objects where needed.
+	// If the lastName was removed, then the last object is not set yet:
+	for (var i = 0; i < names.length; i++)
+	{
+		base = base[names[i]] = base[names[i]] || {};
+	}
+	
+	// If a value was given, set it to the last name:
+	if (lastName) base = base[lastName] = value;
+};
+
+/**
+ * Function: globalise.autoload
+ * =============================================================================
+ * Okay so this function is designed to be called for each lib directory
+ * that the application wants to autoload. Please don't get confused.
+ * Simply by requiring globalise it is enough to globalise all the core node
+ * modules along with any installed modules int he applications node_modules
+ * folder.
+ * 
+ * What this does is provides a PHPish way of doing autoloading.
+ * You tell me where your lib folder is and assuming it contains a folder for
+ * each part of the namespace and a file for the final part of the function or
+ * class name we will automatically require those files for you.
+ * 
+ * Sorry that sounds really long winded I know...
+ * 
+ * Parameters:
+ * -----------------------------------------------------------------------------
+ * dir - the directory to autoload
+ * 
+ * Returns:
+ * -----------------------------------------------------------------------------
+ * void
+ */
+Namespace('globalise.autoload', function(dir)
+{
+	// Traverse the lib folder recursively
+	file.walkSync(dir, function(start, dirs, files)
+	{
+		// Loop through the files that we find
+		files.forEach(function(file)
+		{
+			// Require the file
+			require(path.resolve(start+'/'+file));
+		});
+	});
+});
+
+// Here are all the core node modules.
+// With a few exceptions that we need not worry about.
+var core_modules =
+[
+	'assert', 'buffer', 'child_process', 'cluster', 'console', 'constants',
+	'crypto', 'dgram', 'dns', 'domain', 'events', 'freelist', 'fs', 'http',
+	'https', 'net', 'os', 'path', 'punycode', 'querystring', 'readline',
+	'repl', 'stream', 'string_decoder', 'tls', 'tty', 'url', 'util', 'vm',
+	'zlib'
+];
+
+// Lets loop through all those and require them
+// and add the export to the global scope.
+core_modules.forEach(function(module){ Namespace(module, require(module)); });
+
+// Now we need to load all the installed modules, only local to this app
+fs.readdirSync('./node_modules').forEach(function(module)
+{
+	// We don't want to include ourselves again
+	if (module != 'globalise')
+	{
+		// Require the module and load what it exports into the global scope
+		Namespace(module, require(module));
+	}
+});
